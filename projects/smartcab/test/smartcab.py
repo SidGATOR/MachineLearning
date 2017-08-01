a9b6ac52b44ce834af2ed057db463a7a6c9484c7
Siddharth Sharma <siddharthsharma@ufl.edu>
2017-08-01 00:51:47 -0700

    A Smartcab trained using reinforcement learning technique

---
+++ projects/smartcab/test/smartcab.py
@@ -0,0 +1,11 @@
+import visuals as vs
+
+
+vs.plot_trials('sim_no-learning.csv')
+
+
+# Load the 'sim_default-learning' file from the default Q-Learning simulation
+vs.plot_trials('sim_default-learning.csv')
+
+# Load the 'sim_improved-learning' file from the improved Q-Learning simulation
+vs.plot_trials('sim_improved-learning.csv')
